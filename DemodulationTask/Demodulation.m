e099c83c3286e1ffd079c0c5a6d6756e2dd73c63
Dmitry <ksenon.104@gmail.com>
2024-06-17 14:25:04 +0300

    Comments added

--- DemodulationTask/Demodulation.m
+++ DemodulationTask/Demodulation.m
@@ -23,14 +23,24 @@ fclose(fid);
 % комплексные числа
 x = complex(y(1,:),y(2,:));
 
+% В файле записан комплексный сигнал на нулевой частоте. Т.е. по сути в
+% файле записана комплексная огибающая сигнала. Поэтому достаточно взять
+% действительную часть abs(x).*cos(angle(x)) или real(x)
 
-% sound(real(x), Fs_am);
+% sound(abs(x).*cos(angle(x)), Fs_am);  # Перед проигрыванием уменьшить
+% звук компьютера!!
 % figure
 % plot(real(x(1:1000)));
 
 %% Демодуляция FM сигнала
+
+% Частота дискретизации FM сигнала
 % Fs_fm = 500*1e6;
+% Так как матлаб в функции sound не позволяет использовать число > 1e6,
+% используем 1e6
 Fs_fm = 100000;
+
+% Получение комплексных отсчетов аналогично АМ
 nsamps = (1024*1024*2);
 nstart = 1;
 file_FM = fopen('file1EuropaPlus.bin');
@@ -48,6 +58,6 @@ ComplexSamples = complex(RealParts, ImagParts);
 s_m = atan2(ImagParts, RealParts);
 s_m = gradient(s_m);
 
-figure
-plot(s_m(1:100000));
+% figure
+% plot(s_m(1:100000));
 % sound(s_m, Fs_fm);
