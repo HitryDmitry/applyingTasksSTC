e0d7023cd8092977df557a38445555d042e54fef
Dmitry <ksenon.104@gmail.com>
2024-07-01 08:53:05 +0300

    Reading and printing from dat file

--- Demodulation.cpp
+++ Demodulation.cpp
@@ -1,10 +1,18 @@
+#include <fstream>
+#include <vector>
+#include <complex>
 #include <iostream>
-#include <ifstream>
 
 
 int main(){
-    std::ifstream ifs("file.dat", std::ios::binary | std::ios::in);
+    std::ifstream ifs("DemodulationTask/am_sound.dat", std::ios::binary | std::ios::in);
     std::vector<std::complex<float>> v(3);
-    ifs.read(reinterpret_cast<char*>(v.data()), 6*sizeof(float));
+    ifs.read(reinterpret_cast<char*>(v.data()), 6*sizeof(__int32));
     ifs.close();
+
+    std::cout << v[0] << " " << v[1] << " " << v[2] << std::endl;
+    // for (auto c_value: v){
+    //     std::cout << c_value.real() << " " << c_value.imag() << std::endl;
+    // }
+
 }
